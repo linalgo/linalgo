3a8786e5ba5f1ea3ce8974bdd07763dd6df7b821
Arnaud Rachez <arnaud@linalgo.com>
2025-03-28 23:53:12 +0900

    feat: add document display

--- linalgo/annotate/display.py
+++ linalgo/annotate/display.py
@@ -14,19 +14,54 @@ def init():
     .underline {
         border-bottom: 1px dotted grey;
         cursor: pointer;
+        margin-right: 3px;
     }
     .tooltip {
+        white-space: pre;
         display: none;
-        background: #333;
+        background: black;
         color: white;
-        font-weight: bold;
         padding: 4px 8px;
         font-size: 13px;
         border-radius: 4px;
+        max-width: 400px;
+        text-wrap: wrap;
     }
 
     .tooltip[data-show] {
         display: block;
+    }
+    .arrow,
+    .arrow::before {
+        position: absolute;
+        width: 8px;
+        height: 8px;
+        background: inherit;
+    }
+
+    .arrow {
+        visibility: hidden;
+    }
+
+    .arrow::before {
+        visibility: visible;
+        content: '';
+        transform: rotate(45deg);
+    }
+    .tooltip[data-popper-placement^='top'] > .arrow {
+        bottom: -4px;
+    }
+
+    .tooltip[data-popper-placement^='bottom'] > .arrow {
+        top: -4px;
+    }
+
+    .tooltip[data-popper-placement^='left'] > .arrow {
+        right: -4px;
+    }
+
+    .tooltip[data-popper-placement^='right'] > .arrow {
+        left: -4px;
     }
 </style>
 <script type="text/javascript" src="https://storage.googleapis.com/linhub.linalgo.com/linalgo.2.umd.js"></script>
@@ -38,26 +73,45 @@ function annotate(selector, annotations) {
     const annotator = new Annotator(doc);
     for (annotation of annotations) {
         annotator.showAnnotation(annotation);
-        const el = document.querySelector(`[id='${annotation.id}']`);
-        el.classList.add("underline");
-        const node = document.createElement("pre");
-        node.setAttribute("role", "tooltip");
-        const textnode = document.createTextNode(JSON.stringify(annotation.body, null, 2));
-        node.appendChild(textnode);
-        node.classList.add("tooltip");
-        el.appendChild(node);
-        const popperInstance = Popper.createPopper(el, node, {
-            placement: 'bottom',
-        });
-        function show() {
-            if (node.hasAttribute('data-show')) {
-                node.removeAttribute('data-show');
-            } else {
-                node.setAttribute('data-show', '');
+        const elements = document.querySelectorAll(`[id='${annotation.id}']`);
+        elements.forEach(el => {
+            el.classList.add("underline");
+            if (!el.textContent) {
+                el.remove()
             }
-            popperInstance.update();
-        }
-        el.addEventListener('click', show);
+        });
+        const node = document.createElement("div");
+        const textnode = document.createTextNode(JSON.stringify(annotation.body, null, 2));
+        const arrow = document.createElement("div");
+        node.setAttribute("role", "tooltip");
+        node.classList.add("tooltip");
+        arrow.setAttribute("data-popper-arrow", "");
+        arrow.classList.add("arrow");
+        doc.appendChild(node);
+        node.appendChild(textnode);
+        node.appendChild(arrow);
+        elements.forEach(el => {
+            const popperInstance = Popper.createPopper(el, node, {
+                placement: 'bottom',
+                modifiers: [
+                    {
+                        name: 'offset',
+                        options: {
+                            offset: [0, 8],
+                        },
+                    },
+                ],
+            })
+            function show() {
+                if (node.hasAttribute('data-show')) {
+                    node.removeAttribute('data-show');
+                } else {
+                    node.setAttribute('data-show', '');
+                }
+                popperInstance.update();
+            }
+            el.addEventListener('click', show)
+        });
     }
 }
 </script>
